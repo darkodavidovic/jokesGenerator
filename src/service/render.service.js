efe823b585daa63e962155e978f2bed3777b5f0c
Darko Davidovic <darko.davidovic@comdata.rs>
2024-01-16 12:52:30 +0100

    removed serverurl from render

--- src/service/render.service.js
+++ src/service/render.service.js
@@ -5,7 +5,6 @@ import { appScreen, jokeContainer, loginScreen } from "../selectors";
 class RenderService {
 
     constructor() {
-        this.serverURL = "http://softinz20233-001-site1.gtempurl.com/api/users";
         this.jokesURL = "https://icanhazdadjoke.com/"
     }
 
